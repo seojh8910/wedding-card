5d28f9e6c4be267b89654aa1b1832c2480014926
jihoonseo <seojh8910@gmail.com>
2024-04-18 15:37:24 +0900

    리베이스 테스트

--- account/views.py
+++ account/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def create_account(request):
+
+    return render(request, 'base.html')
--- wedding_card/settings.py
+++ wedding_card/settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'wedding_card.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -75,6 +75,8 @@ TEMPLATES = [
         },
     },
 ]
+
+
 
 WSGI_APPLICATION = 'wedding_card.wsgi.application'
 
