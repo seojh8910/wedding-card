81cc08f4a0c61a08a71f59305f2f583c3ed15600
jihoonseo <seojh8910@gmail.com>
2024-04-18 16:29:43 +0900

    로그인 템플릿 추가(#3)

--- account/views.py
+++ account/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def hello_world(request):
     return render(request, 'account/hello_world.html')
+
+
+def login(request):
+    return render(request, 'account/login.html')
