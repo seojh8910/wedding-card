3f61cbb43585430110567c6f3882f2650a93de71
Hyojung Nam <1326cy@gmail.com>
2024-04-23 15:00:38 +0900

    change: USER 모델 로그인 필드 수정  (#2)

--- account/models.py
+++ account/models.py
@@ -7,7 +7,12 @@ class User(AbstractUser):
     가입 유형은 4가지
     gmail, naver, kakao, basic
     """
+    username = models.CharField(max_length=20, null=False)
     first_name = None
     last_name = None
-    phone_number = models.CharField(max_length=30, null=False, unique=True)
+    email = models.EmailField(max_length=254, unique=True)
+    phone_number = models.CharField(max_length=30, null=False)
     type = models.CharField(max_length=30, null=False, default='basic')
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
