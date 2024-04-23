d0658ed6bf1af03ae79dd22f8d0f06b45d37b259
jihoonseo <seojh8910@gmail.com>
2024-04-23 12:54:41 +0900

    fix: 가입유형에 따라 올바르게 저장하도록 수정(#2)

--- account/forms.py
+++ account/forms.py
@@ -1,12 +1,14 @@
 from django.contrib.auth.forms import UserCreationForm
+from django import forms
 
 from account.models import User
 
 
 class CreateUserForm(UserCreationForm):
+    type = forms.CharField(widget=forms.HiddenInput(),initial='basic')
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2', 'phone_number']
+        fields = ['username', 'email', 'password1', 'password2', 'phone_number', 'type']
         error_messages = {
             'password_mismatch': ('비밀번호 확인이 맞지 않습니다.'),
         }
