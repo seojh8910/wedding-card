173d43d61c621a6fb09299656c44f2130309de9a
jihoonseo <seojh8910@gmail.com>
2024-04-24 13:36:35 +0900

    add: Board 모델 추가(#19)

--- board/models.py
+++ board/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+from account.models import User
+
+
+class Board(models.Model):
+
+    writer = models.ForeignKey(User, on_delete=models.CASCADE, related_name='board')
+    title = models.CharField(max_length=100)
+    content = models.TextField(max_length=100)
+    is_secret = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
