9eece41e875f7bdd76fc3e4cf6fbd36ce1746c28
jihoonseo <seojh8910@gmail.com>
2024-04-30 14:36:03 +0900

    add: 댓글모델 추가

--- board/models.py
+++ board/models.py
@@ -11,3 +11,11 @@ class Board(models.Model):
     is_secret = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+
+class Comment(models.Model):
+    board = models.ForeignKey(Board, on_delete=models.CASCADE, related_name='comment')
+    writer = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comment')
+    content = models.TextField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
