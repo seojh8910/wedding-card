4350ea6e6bb1e7192ae6413b6dc0f00ce2f2dff8
jihoonseo <seojh8910@gmail.com>
2024-05-01 14:09:06 +0900

    add: Review 모델 추가(#24)

--- review/models.py
+++ review/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+from account.models import User
+
+
+class Review(models.Model):
+    writer = models.ForeignKey(User, on_delete=models.CASCADE, related_name='reviews')
+    image = models.ImageField(upload_to='reviews')
+    content = models.CharField(max_length=150)
+    created_at = models.DateTimeField(auto_now_add=True)
