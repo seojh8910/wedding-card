1c3ee22186adc89df36c0dc1b2199c16aa77b199
jihoonseo <seojh8910@gmail.com>
2024-05-02 14:15:31 +0900

    feat: 고객리뷰 삭제기능 추가(#24)

--- review/urls.py
+++ review/urls.py
@@ -7,4 +7,5 @@ app_name = 'review'
 urlpatterns = [
     path('list/', views.review_list, name='review_list'),
     path('create/', views.review_create, name='review_create'),
+    path('delete/<int:pk>', views.review_delete, name='review_delete'),
 ]
--- review/views.py
+++ review/views.py
@@ -24,3 +24,18 @@ def review_create(request):
             return redirect('review:review_create')
     form = ReviewCreationForm
     return render(request, 'review/create.html', context={'form': form})
+
+
+def review_delete(request, pk):
+    target_review = Review.objects.filter(pk=pk).first()
+    if not target_review:
+        messages.error(request, '존재하지않는 고객 리뷰입니다.')
+        return redirect('review:review_list')
+
+    if request.user == target_review.writer or request.user.is_staff:
+        target_review.delete()
+        messages.success(request, '리뷰가 삭제되었습니다.')
+    else:
+        messages.error(request, '리뷰 삭제 권한이 없습니다.')
+
+    return redirect('review:review_list')
