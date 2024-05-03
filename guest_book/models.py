f94e7e96a62de7f74255e68053452104a7e7e07b
Hyojung Nam <1326cy@gmail.com>
2024-05-03 15:04:04 +0900

    BE add: GuestBook 모델 추가 (#22)

--- guest_book/models.py
+++ guest_book/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+from account.models import User
+from card.models import Card
+
+
 # Create your models here.
+class GuestBook(models.Model):
+    card = models.ForeignKey(Card, on_delete=models.CASCADE, related_name='guestbooks')
+    writer = models.CharField(max_length=30)
+    password = models.CharField(max_length=30)
+    content = models.TextField(null=False)
+    created_at = models.DateField(auto_now_add=True)
--- wedding_card/settings.py
+++ wedding_card/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'board',
     'card',
     'review',
+    'guest_book',
 ]
 
 MIDDLEWARE = [
