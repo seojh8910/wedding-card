50357253bbddd5f8e15378f2dd1f834f6a27dc7a
jihoonseo <seojh8910@gmail.com>
2024-05-17 15:37:41 +0900

    FE add: ajax csrftoken 파일 추가

---
+++ static/js/csrftoken.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+    // CSRF token 가져오기
+    function getCookie(name) {
+        let cookieValue = null;
+        if (document.cookie && document.cookie !== '') {
+            const cookies = document.cookie.split(';');
+            for (let i = 0; i < cookies.length; i++) {
+                const cookie = cookies[i].trim();
+                // Does this cookie string begin with the name we want?
+                if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                    break;
+                }
+            }
+        }
+        return cookieValue;
+    }
+
+    const csrftoken = getCookie('csrftoken');
+
+    // CSRF 토큰을 모든 AJAX 요청에 포함
+    $.ajaxSetup({
+        beforeSend: function (xhr, settings) {
+            if (!this.crossDomain) {
+                xhr.setRequestHeader("X-CSRFToken", csrftoken);
+            }
+        }
+    });
+});
