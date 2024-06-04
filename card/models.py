4b9bdf884fe3d4c2a008b1ada8825f37ded78c5e
Hyojung Nam <1326cy@gmail.com>
2024-06-05 00:22:53 +0900

    BE add: 예식장명, 층과 홀 필드 및 blank=True 추가 (#22)

--- card/models.py
+++ card/models.py
@@ -39,31 +39,33 @@ class Card(models.Model):
     ]
 
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='card', null=True)
-    theme = models.CharField(max_length=20, choices=THEME_TYPE, null=True, default='클래식')  # 청첩장 테마
+    theme = models.CharField(max_length=20, choices=THEME_TYPE, default='클래식')  # 청첩장 테마
 
-    wedding_date = models.DateTimeField(null=True)  # 결혼식 날짜
-    wedding_hall_address = models.CharField(max_length=100, null=True)  # 예식장 주소
-    main_img = models.ImageField(upload_to='card/main/', null=True)  # 청첩장 메인 이미지
+    wedding_date = models.DateTimeField(null=True, blank=True)  # 결혼식 날짜
+    wedding_hall_name = models.CharField(max_length=100, null=True, blank=True)  # 예식장명
+    wedding_hall_floor = models.CharField(max_length=100, null=True, blank=True)  # 예식장 층과 홀
+    wedding_hall_address = models.CharField(max_length=100, null=True, blank=True)  # 예식장 주소
+    main_img = models.ImageField(upload_to='card/main/', null=True, blank=True)  # 청첩장 메인 이미지
 
-    groom_name = models.CharField(max_length=20, null=True)  # 신랑 이름
-    groom_mother_name = models.CharField(max_length=20, null=True)  # 신랑 어머니 성함
-    groom_father_name = models.CharField(max_length=20, null=True)  # 신랑 아버지 성함
+    groom_name = models.CharField(max_length=20, null=True, blank=True)  # 신랑 이름
+    groom_mother_name = models.CharField(max_length=20, null=True, blank=True)  # 신랑 어머니 성함
+    groom_father_name = models.CharField(max_length=20, null=True, blank=True)  # 신랑 아버지 성함
     groom_position = models.CharField(max_length=10, choices=GROOM_POSITION, default='아들')  # 신랑 호칭 ex) 아들, 장남, 차남 등
 
-    bride_name = models.CharField(max_length=20, null=True)  # 신부 이름
-    bride_mother_name = models.CharField(max_length=20, null=True)  # 신부 어머니 성함
-    bride_father_name = models.CharField(max_length=20, null=True)  # 신부 아버지 성함
+    bride_name = models.CharField(max_length=20, null=True, blank=True)  # 신부 이름
+    bride_mother_name = models.CharField(max_length=20, null=True, blank=True)  # 신부 어머니 성함
+    bride_father_name = models.CharField(max_length=20, null=True, blank=True)  # 신부 아버지 성함
     bride_position = models.CharField(max_length=10, choices=BRIDE_POSITION, default='딸')  # 신부 호칭 ex) 딸, 장녀, 차녀 등
 
-    invitation_title = models.CharField(max_length=20, null=True)  # 모시는 글 제목
-    invitation_content = models.TextField(null=True)  # 모시는 글 내용
+    invitation_title = models.CharField(max_length=20, null=True, blank=True)  # 모시는 글 제목
+    invitation_content = models.TextField(null=True, blank=True)  # 모시는 글 내용
 
-    thumb_img = models.ImageField(upload_to='card/thumb/', null=True)  # 썸네일 이미지
-    thumb_title = models.CharField(max_length=20, null=True)  # 썸네일 제목
-    thumb_content = models.CharField(max_length=100, null=True)  # 썸네일 내용
+    thumb_img = models.ImageField(upload_to='card/thumb/', null=True, blank=True)  # 썸네일 이미지
+    thumb_title = models.CharField(max_length=20, null=True, blank=True)  # 썸네일 제목
+    thumb_content = models.CharField(max_length=100, null=True, blank=True)  # 썸네일 내용
 
-    guests_comment = models.CharField(max_length=20, null=True, default='temp')  # 임시 / 하객 방명록
+    guests_comment = models.CharField(max_length=20, null=True, default='temp')  # 하객 방명록
 
-    created_at = models.DateTimeField(auto_now_add=True, null=True)  # 청첩장 생성일
-    updated_at = models.DateTimeField(auto_now=True, null=True)  # 청첩장 수정일
+    created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)  # 청첩장 생성일
+    updated_at = models.DateTimeField(auto_now=True, null=True, blank=True)  # 청첩장 수정일
 
