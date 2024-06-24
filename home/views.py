033c3854d28c7171152cdb90e72956acc62fd1c3
jihoonseo <seojh8910@gmail.com>
2024-06-24 21:51:17 +0900

    BE feat: 메인페이지에 고객 리뷰 실제리뷰로 출력되도록 변경

--- home/views.py
+++ home/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
 
+from review.models import Review
+
+
 def landing_page(request):
-    return render(request, 'home/index.html')
+    review_list = Review.objects.all().order_by('-created_at')[:8]
+    context = {'review_list': review_list}
+    return render(request, 'home/index.html', context)
