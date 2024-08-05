811db1cec111d7d35948a58219a83baec3c7bc00
Hyojung Nam <1326cy@gmail.com>
2024-08-06 00:46:03 +0900

    BE update: type 컬럼에 blank=True 추가

--- account/models.py
+++ account/models.py
@@ -12,7 +12,7 @@ class User(AbstractUser):
     last_name = None
     email = models.EmailField(max_length=254, unique=True)
     phone_number = models.CharField(max_length=30, null=False)
-    type = models.CharField(max_length=30, null=False, default='basic')
+    type = models.CharField(max_length=30, blank=True, null=False, default='basic')
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
